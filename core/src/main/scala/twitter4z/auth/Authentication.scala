6d835de8f5d8c736bdbd128bdb400c9e44d11213
ねこはる <halcat0x15a@gmail.com>
2012-02-27 04:32:16 +0900

    delete files

--- core/src/main/scala/twitter4z/auth/Authentication.scala
+++
@@ -1,9 +0,0 @@
-package twitter4z.auth
-
-import twitter4z.http._
-
-sealed trait Authentication
-
-case class Required(consumer: Token[Consumer], access: Token[Access]) extends Authentication
-
-case object Optional extends Authentication
--- core/src/main/scala/twitter4z/http/TokenType.scala
+++
@@ -1,9 +0,0 @@
-package twitter4z.http
-
-sealed trait TokenType
-
-sealed trait Consumer extends TokenType
-
-sealed trait Request extends TokenType
-
-sealed trait Access extends TokenType
